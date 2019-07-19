b93e067931c3ac34d016f18f05423a2e8456423c
Glitch (seemly-feast) <none>
2019-07-19 03:43:23 +0000

    🍛🛣 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,48 @@
+'use strict';
+
+var express = require('express');
+var cors = require('cors');
+
+// require and use "multer"...
+
+var app = express();
+var multer  = require('multer');
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+     res.sendFile(process.cwd() + '/views/index.html');
+  });
+
+// SET STORAGE
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now())
+  }
+})
+ 
+var upload = multer({ storage: storage });
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res, next) => {
+  const file = req.file;
+  if (!file) {
+    const error = new Error('Please upload a file');
+    error.httpStatusCode = 400;
+    return next(error);
+  } 
+    res.send({name: file.originalname, type: file.mimetype, size: file.size});
+  
+})
+
+
+app.get('/hello', function(req, res){
+  res.json({greetings: "Hello, API"});
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Node.js listening ...');
+});
